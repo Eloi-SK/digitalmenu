791333d8983776c21d6cf129a82dda67f48197b8
Eloi Matos <eloi_matos@hotmail.com>
2021-09-07 09:15:57 -0300

    fix(adicionando o annotation @Component a classe AuthEntryPointJwt)

--- src/main/java/com/eloi/digitalmenu/security/jwt/AuthEntryPointJwt.java
+++ src/main/java/com/eloi/digitalmenu/security/jwt/AuthEntryPointJwt.java
@@ -10,10 +10,13 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.web.AuthenticationEntryPoint;
+import org.springframework.stereotype.Component;
 
+@Component
 public class AuthEntryPointJwt implements AuthenticationEntryPoint {
 
     private static final Logger logger = LoggerFactory.getLogger(AuthEntryPointJwt.class);
+
     @Override
     public void commence(HttpServletRequest request, HttpServletResponse response,
             AuthenticationException authException) throws IOException, ServletException {
